493e702d0975ab559de9a12044223e30026ccf2a
Chris & Oscar <chris-oscar@aa.io>
2015-08-17 14:07:24 -0400

    flesh out user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+
+  def index
+    render json: User.all
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user
+    else
+      render(
+        json: user.errors.full_messages, status: :unprocessable_entity
+      )
+    end
+  end
+
+  def show
+    render json: User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+
+    render json: user
+  end
+
+  def destroy
+    users = User.all
+    users.destroy(params[:id])
+
+    render json: users
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
