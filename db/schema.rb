c3d44b52525f24b0d45c7408c2381c4068588d2e
Chris & Oscar <chris-oscar@aa.io>
2015-08-17 14:14:37 -0400

    add unique constraint to username'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817180552) do
+ActiveRecord::Schema.define(version: 20150817181322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,5 +21,7 @@ ActiveRecord::Schema.define(version: 20150817180552) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
