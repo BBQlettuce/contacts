cebddcc465922a3a99a8bc9974d70cc40b32d990
Chris & Oscar <chris-oscar@aa.io>
2015-08-17 14:53:35 -0400

    add ContactShare model and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817182735) do
+ActiveRecord::Schema.define(version: 20150817184352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contact_shares", force: :cascade do |t|
+    t.integer  "contact_id", null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "contact_shares", ["contact_id", "user_id"], name: "index_contact_shares_on_contact_id_and_user_id", unique: true, using: :btree
+  add_index "contact_shares", ["contact_id"], name: "index_contact_shares_on_contact_id", using: :btree
+  add_index "contact_shares", ["user_id"], name: "index_contact_shares_on_user_id", using: :btree
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name",       null: false
@@ -25,7 +36,7 @@ ActiveRecord::Schema.define(version: 20150817182735) do
   end
 
   add_index "contacts", ["email", "user_id"], name: "index_contacts_on_email_and_user_id", unique: true, using: :btree
-  add_index "contacts", ["user_id"], name: "index_contacts_on_user_id", unique: true, using: :btree
+  add_index "contacts", ["user_id"], name: "index_contacts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username",   null: false
